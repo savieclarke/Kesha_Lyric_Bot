267037c7bbb11f212abe922d4228c6c86dd3bc23
Savannah Clarke <92831573+savieclarke@users.noreply.github.com>
2022-06-27 18:09:59 -0400

    post

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("it's working");
+console.log("the bot is starting");
 
 
 var Twit = require('twit');
@@ -11,4 +11,22 @@ consumer_secret : 'process.env.consumer_secret',
 access_token : 'process.env.access_token',
 access_token_secret : 'process.env.access_token_secret',
 });
+
+
+var tweet = {
+    status: 'We were lovers in a past life'
+}
+
+T.post('statuses/update', tweet, tweeted);
+
+function tweeted (err, data, response) {
+console.log(data)
+    if (err) {
+        console.log("something went wrong!");
+
+    } else {
+        console.log("it worked!");
+    }
+}
+
 
