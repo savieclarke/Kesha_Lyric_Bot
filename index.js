f3dd258cdfc3245641a844dda33d61d43fd3b4db
Savannah Clarke <92831573+savieclarke@users.noreply.github.com>
2022-06-27 16:08:59 -0400

    process.env

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ require('dotenv').config();
 
 console.log(process.env);
 
+var T = new Twit({
+    consumer_key: process.env.consumer_key,
+    consumer_secret: process.env.consumer_secret,
+    access_token: process.env.access_token,
+    access_token_secret: process.env.access_token_secret,
+  })
