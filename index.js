8c3b900bfae1c3995803d206fa55a5924fcefaf9
Savannah Clarke <92831573+savieclarke@users.noreply.github.com>
2022-06-27 19:15:11 -0400

    new package

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 console.log("the bot is starting");
 
 
-var Twit = require('twit');
+const { config } = require('dotenv');
+import { TwitterApi } from 'twitter-api-v2';
+
 require('dotenv').config();
 
 
 var T = new Twit ({
-consumer_key : 'process.env.consumer_key',
-consumer_secret : 'process.env.consumer_secret',
-access_token : 'process.env.access_token',
-access_token_secret : 'process.env.access_token_secret',
+//consumer_key : 'process.env.consumer_key',
+//consumer_secret : 'process.env.consumer_secret',
+ACCESS_TOKEN : 'process.env.ACCESS_TOKEN',
+ACCESS_TOKEN_SECRET : 'process.env.ACCESS_TOKEN_SECRET',
+API_KEY : 'process.env.API_KEY',
+API_KEY_SECRET : 'process.env.API_KEY_SECRET',
+BEARER_TOKEN:  'process.env.BEARER_TOKEN'
 });
-
+ 
 
 var tweet = {
     status: 'We were lovers in a past life'
