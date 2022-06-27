945197d53b1eea9687389445c7221033e78fd794
Savannah Clarke <92831573+savieclarke@users.noreply.github.com>
2022-06-27 17:57:04 -0400

    keys and access

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 console.log("it's working");
 
+
 var Twit = require('twit');
 require('dotenv').config();
 
-console.log(process.env);
 
-var T = new Twit({
-    consumer_key: process.env.consumer_key,
-    consumer_secret: process.env.consumer_secret,
-    access_token: process.env.access_token,
-    access_token_secret: process.env.access_token_secret,
-  })
+var T = new Twit ({
+consumer_key : 'process.env.consumer_key',
+consumer_secret : 'process.env.consumer_secret',
+access_token : 'process.env.access_token',
+access_token_secret : 'process.env.access_token_secret',
+});
+
